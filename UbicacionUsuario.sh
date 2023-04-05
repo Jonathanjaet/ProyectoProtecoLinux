87e7b4597ea999a9c0baf66866a6596e649e32d2
Jonathanjaet <79382765+Jonathanjaet@users.noreply.github.com>
2023-04-05 14:38:36 -0600

    Update UbicacionUsuario.sh
    Modificaciones y aclaraciones, borre cosas que no servian (script Principal)

--- UbicacionUsuario.sh
+++ UbicacionUsuario.sh
@@ -1,68 +1,61 @@
 #!/bin/bash
+# primer script
+
+sudo login
+
+#scomandos del tercer script
+
+trap ctrl_c INT
+
+# comandos ayuda de sentencias
+
 declare -i salida=0
 
 declare -i opt=1
 
 comando=""
+#comandos del cuarto script
+fecha=$(date "+Hoy es: %A %d %B%n Hora: %T")
 
-fecha=$(date +"Hoy es: %A %d %B")
+ram=$(free -t --mega; grep 'MemTotal' /proc/meminfo; grep 'SwapTotal' /proc/meminfo)
 
-ram=$(free -t --mega; grep 'MemTotal' /proc/meminfo; grep 'SwapTotal' /proc/mem>
-
-arqui=$(sudo lshw -C CPU | grep width)
+arqui=$(uname -m)
 
 versionSO=$(lsb_release -idc)
-# se cancela el uso del ctrl+c mientras ejecuta el script
-trap ctrl_c INT 
-# inicio del segundo script
+
+
 while [ $opt != $salida ]
 do
-    echo $USER
-    pwd
-    
-    echo "Tu decides si sales $USER"
-    echo "Tus opciones de comandos son: "
-    echo "[ ram, arqui, versionSO , fechaElegante ]" 
-    read -p "Ingrese el comando a leer: " comando
-    # aun por arreglar el if
-    if [ $comando == "fechaElegante" ]
-    then
-           echo "$fechaElegante"
-    elif [ $comando == "ram" ]
-           echo "$ram"
-    elif [ $comando == "versionSO" ]
-           echo "$versionSO"
-    elif [ $comando == "arqui" ]
-           echo "$arqui"
-    fi
-    read -p "Desea salir [1] o continuar[0]: " salida
-    echo "$INTRO"
-    
+        
+        #Segundo script
+        pwd
+        echo "Hola $USER"
+
+        echo "Tu decides si sales $USER\n"
+        echo "Tus opciones de comandos son: "
+        echo "[ ram, arqui, versionSO , fecha ]" 
+        read -p "Ingrese el comando a leer: " comando
+
+        if [ $comando == "fecha" ]
+        then
+                echo "$fecha"
+        elif [ $comando == "ram" ]
+        then
+                echo "$ram"
+        elif [ $comando == "versionSO" ]
+        then
+                echo "$versionSO"
+        elif [ $comando == "arqui" ]
+        then
+                echo "$arqui"
+        else
+                echo "Opcion invalida, ingrese una opcion valida"
+        fi
+
+        read -p "Desea salir [1] o continuar[0]: " salida
+        echo "$INTRO"
+        clear
 done
 
-# inicio del tercer script
+echo "Vuelve pronto!"
 
-cat .bashrc
-
-echo 'alias lm="ls -la"' >> .bashrc
-
-source ~/.bashrc
-
-cat .bashrc
-
-# no funciona aún
-
-echo 'alias yaquedo="cd ..; ls -la"' >> .bashrc
-
-cat .bashrc
-
-source .bashrc
-
-
-PATH=$(dirname "${BASH_SOURCE[0]}")/bin:$PATH
-export PATH
-porfin() {
-    cd ..; ls -la
-}
- # requiere confirmar con source ./bashrc
-# se intentó de otra forma y no funcionó, seguire intentando y borrare mañana lo que no funcionó
