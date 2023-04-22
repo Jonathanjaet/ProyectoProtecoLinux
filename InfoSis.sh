4f516b0325b166a045b1dc64d3e50560f4ac7507
Jonathanjaet <79382765+Jonathanjaet@users.noreply.github.com>
2023-04-22 14:17:01 -0600

    Update InfoSis.sh
    Agregue diseño al infosis.sh

--- InfoSis.sh
+++ InfoSis.sh
@@ -2,12 +2,19 @@
 
 ## ram
 
-free -t --mega; grep 'MemTotal' /proc/meminfo; grep 'SwapTotal' /proc/meminfo
+echo "Se muestra la memoria ram disponible: "
+echo "______________________________________"
 
+free -t --mega; grep 'MemTotal' /proc/meminfo; grep 'SwapTotal' /proc/meminfo
+echo "______________________________________"
 ## arquitectura
+echo "La arquitectura de su computadora es: "
+echo "______________________________________"
 
 uname -m
-
+echo "______________________________________"
 ## identificacion de version del SO
-
+echo "La versión de su Sistema Operativo a detalle es: "
+echo "_________________________________________________"
 lsb_release -idc
+echo "_________________________________________________"
